6bec5b88c678ca3da9c2033d4730dfc9748f334d
Siddharaj Yadav <sidharajyadav157@gmail.com>
2023-12-07 15:00:07 +0530

    mongoose conntected

---
+++ src/index.js
@@ -0,0 +1,31 @@
+// import mongoose from "mongoose";
+// import { DB_Name } from "./constacts";
+
+// ( async () => {
+//     try{
+//        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_Name}`)
+//     } catch (error){
+//         console.error("ERROR:",error)
+//         throw err
+//     }
+// })()
+
+// require('dotenv').config({path: './env'})
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+import {app} from './app.js'
+dotenv.config({
+    path: './.env'
+})
+
+
+
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+})
